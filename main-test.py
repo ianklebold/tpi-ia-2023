15e51775b3218394c9a0d715f16c64c982f44e58
JorgeHadad <jorgemenegaz@gmail.com>
2023-09-30 17:37:30 -0300

    fix print results

--- main-test.py
+++ main-test.py
@@ -4,9 +4,31 @@ import math
 # Lee el dataset desde un archivo CSV
 dataset = pd.read_csv('resources\dataset_cleaned_fixed.csv')
 
+# Renombrar las columnas
+dataset.rename(columns={
+    # 'id': 'ID',
+    'age': 'Edad',
+    'education': 'Educación',
+    'sex': 'Sexo',
+    'is_smoking': 'Fumador',
+    'cigsPerDay': 'Cigarrillos por Día',
+    'BPMeds': 'Toma Medicamentos Pre ART',
+    'prevalentStroke': 'Derrame Cerebral',
+    'prevalentHyp': 'Hipertenso',
+    'diabetes': 'Diabetes',
+    'totChol': 'Colesterol Total',
+    'sysBP': 'Presión Arterial Sistólica',
+    'diaBP': 'Presión Arterial Diastólica',
+    'BMI': 'IMC',
+    'heartRate': 'Frecuencia Cardíaca',
+    'glucose': 'Nivel de Glucosa',
+    'TenYearCHD': 'Predicción'
+}, inplace=True)
+
 # Crea una clase para representar cada instancia
 class Instancia:
-    def __init__(self, atributos, clase):
+    def __init__(self, id, atributos, clase):
+        self.id = id
         self.atributos = atributos
         self.clase = clase
 
@@ -17,10 +39,12 @@ instancias = []
 for index, row in dataset.iterrows():
     # Obtiene los atributos de la instancia y los convierte a tipo 'float'
     atributos = [float(value) for value in row[1:-1]]
+    # Obtiene el ID de la instancia
+    id_instancia = int(row['id'])
     # Obtiene la clase de la instancia
     clase = row[-1]
     # Crea una instancia y la agrega a la lista
-    instancia = Instancia(atributos, clase)
+    instancia = Instancia(id_instancia, atributos, clase)
     instancias.append(instancia)
 
 # Función para calcular la distancia entre dos instancias
@@ -41,11 +65,9 @@ def encontrar_vecinos_mas_cercanos(instancias, instancia_nueva, k):
     return vecinos
 
 # Ejemplo de uso
-# atributos_nueva_instancia = [50, 1, 1, 1, 15, 0, 0, 0, 0, 200, 120, 80, 23.0, 75, 85]
-# 35,2,0,1,30,0,0,0,0,245,148.0,84.0,23.74,60,73
-atributos_nueva_instancia = [35,2,0,1,30,0,0,0,0,245,148.0,84.0,23.74,60,73]
-instancia_nueva = Instancia(atributos_nueva_instancia, None)
-k = 5
+atributos_nueva_instancia = [50, 1, 1, 1, 15, 0, 0, 0, 0, 200, 120, 80, 23.0, 75, 85]
+instancia_nueva = Instancia(-1, atributos_nueva_instancia, None)  # Usamos -1 para indicar que no tiene un ID asignado aún
+k = 3
 
 # Encuentra los vecinos más cercanos
 vecinos_mas_cercanos = encontrar_vecinos_mas_cercanos(instancias, instancia_nueva, k)
@@ -59,21 +81,24 @@ vecinos_mas_cercanos.sort(key=lambda x: x.distancia)
 
 # Crea un DataFrame para mostrar los resultados
 df_resultados = pd.DataFrame({
-    'Age': [vecino.atributos[0] for vecino in vecinos_mas_cercanos],
-    'Education': [vecino.atributos[1] for vecino in vecinos_mas_cercanos],
-    'Sex': [vecino.atributos[2] for vecino in vecinos_mas_cercanos],
-    'Is Smoking': [vecino.atributos[3] for vecino in vecinos_mas_cercanos],
-    'Cigs/Day': [vecino.atributos[4] for vecino in vecinos_mas_cercanos],
-    'BPMeds': [vecino.atributos[5] for vecino in vecinos_mas_cercanos],
-    'Prev. Stroke': [vecino.atributos[6] for vecino in vecinos_mas_cercanos],
-    'Prev. Hyp': [vecino.atributos[7] for vecino in vecinos_mas_cercanos],
+    'ID': [vecino.id for vecino in vecinos_mas_cercanos],
+    'Edad': [vecino.atributos[0] for vecino in vecinos_mas_cercanos],
+    'Educación': [vecino.atributos[1] for vecino in vecinos_mas_cercanos],
+    'Sexo': [vecino.atributos[2] for vecino in vecinos_mas_cercanos],
+    'Fumador': [vecino.atributos[3] for vecino in vecinos_mas_cercanos],
+    'Cigarrillos por Día': [vecino.atributos[4] for vecino in vecinos_mas_cercanos],
+    'Toma Medicamentos Pre ART': [vecino.atributos[5] for vecino in vecinos_mas_cercanos],
+    'Derrame Cerebral': [vecino.atributos[6] for vecino in vecinos_mas_cercanos],
+    'Hipertenso': [vecino.atributos[7] for vecino in vecinos_mas_cercanos],
     'Diabetes': [vecino.atributos[8] for vecino in vecinos_mas_cercanos],
-    'Tot Chol': [vecino.atributos[9] for vecino in vecinos_mas_cercanos],
-    'Sys BP': [vecino.atributos[10] for vecino in vecinos_mas_cercanos],
-    'Dia BP': [vecino.atributos[11] for vecino in vecinos_mas_cercanos],
-    'BMI': [vecino.atributos[12] for vecino in vecinos_mas_cercanos],
-    'Heart Rate': [vecino.atributos[13] for vecino in vecinos_mas_cercanos],
-    'Distance': [vecino.distancia for vecino in vecinos_mas_cercanos]
+    'Colesterol Total': [vecino.atributos[9] for vecino in vecinos_mas_cercanos],
+    'Presión Arterial Sistólica': [vecino.atributos[10] for vecino in vecinos_mas_cercanos],
+    'Presión Arterial Diastólica': [vecino.atributos[11] for vecino in vecinos_mas_cercanos],
+    'IMC': [vecino.atributos[12] for vecino in vecinos_mas_cercanos],
+    'Frecuencia Cardíaca': [vecino.atributos[13] for vecino in vecinos_mas_cercanos],
+    'Nivel de Glucosa': [vecino.atributos[14] for vecino in vecinos_mas_cercanos],
+    'Predicción': [vecino.clase for vecino in vecinos_mas_cercanos],
+    'Distancia': [vecino.distancia for vecino in vecinos_mas_cercanos]
 })
 
 # Imprime el DataFrame con los resultados
