097244f6ef8e233f8e1c09005854e4cec4af3c7d
JorgeHadad <jorgemenegaz@gmail.com>
2023-09-30 20:17:34 -0300

    Add new useful prints

--- main-test.py
+++ main-test.py
@@ -51,46 +51,9 @@ for index, row in dataset.iterrows():
     # Crea una instancia y la agrega a la lista
     instancia = Instancia(id_instancia, atributos, clase)
     instancias.append(instancia)
-
-# Función para calcular la distancia entre dos instancias
-def calcular_distancia(instancia1, instancia2):
-    distancia = 0
-    for i in range(len(instancia1.atributos)):
-        distancia += (instancia1.atributos[i] - instancia2.atributos[i]) ** 2
-    return math.sqrt(distancia)
-
-# Función para encontrar los k vecinos más cercanos a una instancia nueva
-def encontrar_vecinos_mas_cercanos(instancias, instancia_nueva, k):
-    distancias = []
-    for instancia in instancias:
-        distancia = calcular_distancia(instancia, instancia_nueva)
-        distancias.append((instancia, distancia))
-    distancias.sort(key=lambda x: x[1])
-    vecinos = [instancia[0] for instancia in distancias[:k]]
-    return vecinos
-
-# Ejemplo de uso
-# atributos_nueva_instancia = [50, 1, 1, 1, 15, 0, 0, 0, 0, 200, 120, 80, 23.0, 75, 85]
-#51,1,1,1,15,0,0,0,0,212,146.0,89.0,24.49,100,132
-atributos_nueva_instancia = [55,1,0,0,0,0,0,1,0,243,142.0,92.0,30.24,70,85]
-instancia_nueva = Instancia(-1, atributos_nueva_instancia, 0.0)  # Usamos -1 para indicar que no tiene un ID asignado aún, Clase Predicción = 1.0 o 0.0
-
-# Lista para almacenar los resultados
-resultados = []
-
-# Iterar para k de 1 a 5
-for k in range(1, 6):
-    # Encuentra los vecinos más cercanos
-    vecinos_mas_cercanos = encontrar_vecinos_mas_cercanos(instancias, instancia_nueva, k)
-
-    # Calcula la distancia para cada vecino
-    for vecino in vecinos_mas_cercanos:
-        vecino.distancia = calcular_distancia(vecino, instancia_nueva)
-
-    # Ordena los vecinos por distancia
-    vecinos_mas_cercanos.sort(key=lambda x: x.distancia)
-    
-    # Crea un DataFrame para mostrar los resultados
+   
+#Mostrar detalles de los vecinos encontrados por instancia en cada K    
+def mostrar_vecinos(vecinos_mas_cercanos, k):
     df_resultados = pd.DataFrame({
         'ID': [vecino.id for vecino in vecinos_mas_cercanos],
         'Edad': [vecino.atributos[0] for vecino in vecinos_mas_cercanos],
@@ -118,19 +81,100 @@ for k in range(1, 6):
     print(df_resultados)
     print("------------------------------------\n")
 
-    # Cuenta cuántas predicciones coinciden con el valor de la clase
-    coincidencias = sum(1 for vecino in vecinos_mas_cercanos if vecino.clase == instancia_nueva.clase)
+# Función para calcular la distancia entre dos instancias
+def calcular_distancia(instancia1, instancia2):
+    distancia = 0
+    for i in range(len(instancia1.atributos)):
+        distancia += (instancia1.atributos[i] - instancia2.atributos[i]) ** 2
+    return math.sqrt(distancia)
 
-    # Almacena los resultados
-    resultados.append({'K': k, 'Coincidencias': coincidencias})
+# Función para encontrar los k vecinos más cercanos a una instancia nueva
+def encontrar_vecinos_mas_cercanos(instancias, instancia_nueva, k):
+    distancias = []
+    for instancia in instancias:
+        distancia = calcular_distancia(instancia, instancia_nueva)
+        distancias.append((instancia, distancia))
+    distancias.sort(key=lambda x: x[1])
+    vecinos = [instancia[0] for instancia in distancias[:k]]
+    return vecinos
 
-# Crea un DataFrame para mostrar los resultados de coincidencias con la clase
-df_coincidencias = pd.DataFrame(resultados)
+# Ejemplo de uso
 
-# Imprime el DataFrame con los resultados de coincidencias
-# print con estrellas ♫
-print("===================================\n")
-print("Resultados de coincidencias con la clase para cada k:")
-print(df_coincidencias)
-print("===================================\n")
+import random
 
+# Obtener 6 instancias aleatorias del dataset
+instancias_random = random.sample(instancias, 6)
+
+# Lista para almacenar los resultados de las instancias aleatorias
+resultados_random = []
+
+# Lista para almacenar los resultados de las coincidencias
+resultados = []
+
+# Lista para almacenar los vecinos más cercanos por cada k para cada instancia
+vecinos_por_k = []
+
+# Lista para almacenar las coincidencias por cada k para cada instancia
+coincidencias_por_k = []
+
+# Iterar para cada instancia aleatoria
+for i, instancia_nueva in enumerate(instancias_random, start=1):
+    # Lista para almacenar los resultados para esta instancia aleatoria
+    resultados_instancia = []
+
+    # Lista para almacenar los vecinos más cercanos para cada k
+    vecinos_k_actual = []
+    coincidencias_k_actual = []
+    
+    # Iterar para k de 1 a 5
+    for k in range(1, 7):
+        # Encuentra los vecinos más cercanos
+        vecinos_mas_cercanos = encontrar_vecinos_mas_cercanos(instancias, instancia_nueva, k)
+
+        # Calcula la distancia para cada vecino
+        for vecino in vecinos_mas_cercanos:
+            vecino.distancia = calcular_distancia(vecino, instancia_nueva)
+
+        # Ordena los vecinos por distancia
+        vecinos_mas_cercanos.sort(key=lambda x: x.distancia)
+        
+        # Crea un DataFrame para mostrar los resultados
+        mostrar_vecinos(vecinos_mas_cercanos, k)
+
+        # Almacena el ID del último vecino más cercano para esta instancia y k
+        vecinos_k_actual.append(vecinos_mas_cercanos[-1].id)
+        
+        # Almacena la cantidad de coincidencias 
+        coincidencias_k_actual.append(sum(1 for vecino in vecinos_mas_cercanos if vecino.clase == instancia_nueva.clase))
+
+        # Cuenta cuántas predicciones coinciden con el valor de la clase
+        coincidencias = sum(1 for vecino in vecinos_mas_cercanos if vecino.clase == instancia_nueva.clase)
+
+        # Almacena los resultados
+        resultados_instancia.append({'K': f'k={k}', 'Coincidencias': coincidencias})
+
+    vecinos_por_k.append([f'k={i}'] + vecinos_k_actual)
+    coincidencias_por_k.append([f'k={i}'] + coincidencias_k_actual)
+
+    # Crea un DataFrame para mostrar los resultados de coincidencias con la clase para esta instancia
+    df_coincidencias_instancia = pd.DataFrame(resultados_instancia)
+    resultados.append({"Instancia": f"i{i}", "Resultados": df_coincidencias_instancia})
+    print(f"\nResultados para la instancia i{i}:")
+    print(df_coincidencias_instancia)
+    
+
+# Ajuste de columnas para el DataFrame de vecinos por k
+columnas = ['K'] + [f'i{i}' for i in range(1, 7)]
+# Añadimos una lista vacía para ajustar las columnas
+vecinos_por_k.append([''] * len(columnas))
+coincidencias_por_k.append([''] * len(columnas))
+
+# Crear el DataFrame para mostrar los vecinos por cada k
+df_vecinos_por_k = pd.DataFrame(vecinos_por_k, columns=columnas)
+print("\nVecinos más cercanos por cada k para cada instancia:")
+print(df_vecinos_por_k)
+
+# Crear el DataFrame para mostrar las coincidencias por cada k
+df_coincidencias_por_k = pd.DataFrame(coincidencias_por_k, columns=columnas)
+print("\nCoincidencias por cada k para cada instancia:")
+print(df_coincidencias_por_k)
