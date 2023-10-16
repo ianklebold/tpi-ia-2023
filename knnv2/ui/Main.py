e99dbecd8a44f1364df271126e68824a020d0fe9
ianklebold <ianstgo@gmail.com>
2023-10-16 18:55:19 -0300

    IU - 004 - Button new instance created

--- knnv2/ui/Main.py
+++ knnv2/ui/Main.py
@@ -12,17 +12,27 @@ def browse_file():
     max = 30
     if file_path:
         if len(file_path) > max:
-            # Recortar a la mitad y agregar puntos de continuación
-            half_length = int(max/2)  # La mitad de 200
+            half_length = int(max / 2)
             truncated_text = file_path[:half_length] + "..."
         else:
             truncated_text = file_path
         choose_label.config(text="File Path: " + truncated_text)
+        messagebox.showinfo("Información", "Archivo seleccionado y guardado con éxito: " + file_path)
     else:
         return None
 
-    messagebox.showinfo("Información", "Archivo seleccionado y guardado con éxito: " + file_path)
-    return file_path
+
+def open_new_window():
+    new_window = tkinter.Toplevel(window)
+    new_window.title("Nueva Instancia")
+    new_window.minsize(X_SIZE, Y_SIZE)
+
+    # Crear un Label en la nueva ventana con la imagen
+    new_label = tkinter.Label(new_window)
+    new_label.place(x=390, y=20)
+    logo = tkinter.PhotoImage(file='images/logo.png')
+    new_label.config(image=logo)
+    new_label.image = logo  # Es importante mantener una referencia a la imagen
 
 
 window = tkinter.Tk()
@@ -48,7 +58,13 @@ file_path_label.place(x=370, y=160)
 browse_button = tkinter.Button(window, text="Browse file", command=browse_file)
 browse_button.place(x=500, y=190)
 
-file_path = None  # Variable para almacenar la dirección del archivo
+# Botón para abrir una nueva ventana
+new_instance_label = tkinter.Label(window, text="Crear nueva instancia")
+new_instance_label.place(x=145, y=290)
+new_window_button = tkinter.Button(window, text="Nueva instancia", command=open_new_window)
+new_window_button.place(x=500, y=300)
+
+file_path = None
 
 window.mainloop()
 
